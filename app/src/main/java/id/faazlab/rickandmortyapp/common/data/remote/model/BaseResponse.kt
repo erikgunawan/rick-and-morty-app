b32525438b68dd04dc2b0db078fbe490dd2a1979
gli-erikgunawan <erik.gunawan44@gmail.com>
2024-12-03 10:03:15 +0700

    add theme

--- app/src/main/java/id/faazlab/rickandmortyapp/common/data/remote/model/BaseResponse.kt
+++ app/src/main/java/id/faazlab/rickandmortyapp/common/data/remote/model/BaseResponse.kt
@@ -1,7 +1,5 @@
 package id.faazlab.rickandmortyapp.common.data.remote.model
 
-import id.faazlab.rickandmortyapp.feature.character.data.remote.model.Character
-
 /**
  * Created by erikgunawan on 02/12/24.
  */
